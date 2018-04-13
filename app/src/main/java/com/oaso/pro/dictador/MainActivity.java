a1e207d1988c74bee50ff900b4c9a410c6ff2804
omarSanOc <omar.saoc@gmail.com>
2018-04-13 12:03:59 -0500

    add TextToSpeech

--- app/src/main/java/com/oaso/pro/dictador/MainActivity.java
+++ app/src/main/java/com/oaso/pro/dictador/MainActivity.java
@@ -2,16 +2,22 @@ package com.oaso.pro.dictador;
 
 import android.annotation.SuppressLint;
 import android.content.Intent;
+import android.speech.tts.TextToSpeech;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.Locale;
+
+public class MainActivity extends AppCompatActivity implements TextToSpeech.OnInitListener{
 
     private int contador = 5;
     private int tiempo = 1;
+
+    private TextToSpeech tts;
 
     private Button plus1;
     private Button plus2;
@@ -28,6 +34,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        tts = new TextToSpeech(this,this);
 
         palabras = findViewById(R.id.palabras);
         tiempoPalabra = findViewById(R.id.tiempo);
@@ -51,7 +59,35 @@ public class MainActivity extends AppCompatActivity {
         onClick();
     }
 
+    @Override
+    public void onInit(int status) {
+
+        if(status == TextToSpeech.SUCCESS){
+
+            int result = tts.setLanguage(Locale.US);
+
+            if(result == TextToSpeech.LANG_MISSING_DATA || result == TextToSpeech.LANG_NOT_SUPPORTED){
+                Log.e("TTS", "This language is not supported");
+            }else{
+                start.setEnabled(true);
+                speakOut();
+
+            }
+        }else{
+            Log.e("TTS", "Initilization Failde!");
+        }
+
+    }
+
     private void onClick(){
+
+        start.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                speakOut();
+            }
+        });
+
         result.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -106,4 +142,12 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
+
+    private void speakOut(){
+        String text = "Hello World";
+        tts.speak(text,TextToSpeech.QUEUE_FLUSH,null);
+
+    }
+
+
 }
